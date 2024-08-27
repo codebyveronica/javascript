5d62c01bcbcda746f742ab4e683c6c88be48becd
Veronica Souza <codebyveronica@gmail.com>
2024-08-27 10:48:04 -0300

    exercicio aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1;
+do { // estrutura de repetição com teste lógico no final
+    console.log(`Passo ${c}`);
+    c++;
+}while (c <= 12);
+
+/* // a mais comum de se encontrar nos programas
+var c = 1; 
+while(c <= 6) { // estrutura de repetição com teste lógico no inicio
+    console.log(`Passp ${c}`);
+    c++;
+}*/
+
+/* // sem teste lógico
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
